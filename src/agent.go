9d8270a34725fea5806d471a4e9609796cd89ed2
komodo <68278515+komodoooo@users.noreply.github.com>
2024-10-30 20:19:14 +0100

    Create agent.go

---
+++ src/agent.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"github.com/fsnotify/fsnotify"
+	vt "github.com/VirusTotal/vt-go"
+)
+
+func main() {
+	client := vt.NewClient(APIKEY)
+	watcher, err := fsnotify.NewWatcher()
+    if err != nil { log.Fatal(err) }
+    defer watcher.Close()
+	err = watcher.Add(os.Getenv("USERPROFILE")+"\\Downloads")
+    if err != nil { log.Fatal(err) }
+	for {
+        select {
+        case event := <-watcher.Events:
+            if event.Op&fsnotify.Create == fsnotify.Create {
+				send_notification("Uploading", event.Name, "")
+				obj, err := UploadFile(event.Name, client)
+				send_notification("Scanning", "Wait a bit", obj.ID())
+				if err != nil { log.Fatalf("Error: %v", err) }
+				stats, n, err := GetAnalysisAttributes(obj, client)
+				if err != nil { log.Fatalf("Error: %v", err) }
+				if stats[0] + stats[1] == 0 {
+					send_notification("File is secure!", "", obj.ID())
+				} else {
+					send_notification("File could be dangerous", fmt.Sprintf("Flagged as Malicios (%d), Suspicious (%d)\nby %d antiviruses", stats[1], stats[0], n), obj.ID())
+				}
+            }
+        case err := <-watcher.Errors:
+            log.Println("Error: ", err)
+        }
+    }
+}
