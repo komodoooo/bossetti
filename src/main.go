70c169e5a7cbc9aecdd6758eb94ebcff8937c638
komodo <68278515+komodoooo@users.noreply.github.com>
2024-11-01 13:13:35 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -3,23 +3,20 @@ package main
 import (
 	"os"
 	"fmt"
-	"log"
 	vt "github.com/VirusTotal/vt-go"
 )
 
 func main() {
-	if len(os.Args)<2{ log.Fatal("Must specify a file") }
 	client := vt.NewClient(APIKEY)
-	filepath := os.Args[0]					//contextual menu pass filepath with arg 0, not 1
+	filepath := os.Args[1]
 	send_notification("Uploading", filepath, "")
-	object, err := UploadFile(filepath, client)
-	if err != nil { log.Fatalf("Error: %v", err) }
+	object, _ := UploadFile(filepath, client)
 	send_notification("Scanning", "Wait a bit", object.ID())
 	stats, n, err := GetAnalysisAttributes(object, client)
-	if err != nil { log.Fatalf("Error: %v", err) }
+	if err != nil { send_notification("Error", err.Error(), object.ID())}
 	if stats[0]+stats[1] == 0 {
 		send_notification("File is secure!", "", object.ID())
 	} else {
-		send_notification("Done", fmt.Sprintf("Flagged as Malicios (%d), Suspicious (%d)\nby %d antiviruses", stats[1], stats[0], n), object.ID())
+		send_notification("Done", fmt.Sprintf("Flagged as Malicious (%d), Suspicious (%d)\nby %d antiviruses", stats[1], stats[0], n), object.ID())
 	}
 }
