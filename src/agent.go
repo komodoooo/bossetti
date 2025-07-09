9e2b1f8e926501853492d857ca36717d2a5cfa1d
komodo <68278515+komodoooo@users.noreply.github.com>
2025-07-09 14:12:40 +0200

    Update agent.go

--- src/agent.go
+++ src/agent.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"slices"
 	"github.com/fsnotify/fsnotify"
 	vt "github.com/VirusTotal/vt-go"
 )
@@ -16,14 +17,20 @@ func main() {
 		select {
 		case event := <-watcher.Events:
 			if event.Op&fsnotify.Create == fsnotify.Create {
-				send_notification("Uploading", event.Name, "")
-				obj, _ := UploadFile(event.Name, client)
-				send_notification("Scanning", "Wait a bit", obj.ID())
-				stats, n, _ := GetAnalysisAttributes(obj, client)
-				if stats[0] + stats[1] == 0 {
-					send_notification("File is secure!", "", obj.ID())
-				} else {
-					send_notification("File could be dangerous", fmt.Sprintf("Flagged as Malicios (%d), Suspicious (%d)\nby %d antiviruses", stats[1], stats[0], n), obj.ID())
+				if len(event.Name)>=3 && slices.Contains(FILE_EXTENSIONS, event.Name[len(event.Name)-3:]){
+					send_notification("Uploading", event.Name, "")
+					obj, err := UploadFile(event.Name, client)
+					if err != nil {
+						send_notification("Error", err.Error(), obj.ID())
+						continue
+					}
+					send_notification("Scanning", event.Name, obj.ID())
+					stats, n, _ := GetAnalysisAttributes(obj, client)
+					if stats[0] + stats[1] == 0 {
+						send_notification("File is secure!", "", obj.ID())
+					} else {
+						send_notification("File could be dangerous", fmt.Sprintf("Flagged as Malicios (%d), Suspicious (%d)\nby %d antiviruses", stats[1], stats[0], n), obj.ID())
+					}
 				}
 			}
 		case err := <-watcher.Errors:
